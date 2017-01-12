463860ff0e381d41030ecb1f711ba68d9167e81c
bugeaggeorge <bugeag.george@live.com>
2017-01-12 21:39:00 +0200

    Update

---
+++ MBBMonopoly/MyForm.h
@@ -0,0 +1,81 @@
+#pragma once
+#include "all.h"
+#include "Meniu.h"
+
+using namespace std;
+
+namespace MBBMonopoly {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1006, 593);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
+			this->MaximumSize = System::Drawing::Size(1024, 640);
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"MBB Monopoly";
+			this->Click += gcnew System::EventHandler(this, &MyForm::MyForm_Click);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+		private: System::Void MyForm_Click(System::Object^  sender, System::EventArgs^  e)
+		{
+			Meniu ^ form = gcnew Meniu();
+			this->Hide();
+			form->Show();
+		}
+	};
+}
